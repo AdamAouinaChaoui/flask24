2f2b2687943519c9c1f010955561e55ffb1f781f
AdamAouinaChaoui <145915167+AdamAouinaChaoui@users.noreply.github.com>
2024-12-04 16:54:22 +0100

    Update sample_app.sh

--- sample_app.sh
+++ sample_app.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-staging exercise with python environment van github
-python3 -m venv flask24
-source flask_venv/bin/activated
-git clone https://github.com/AdamAouinaChaoui/flask24.git
+cleanup() {
+    echo "Cleaning up ..."
+    deactivate
+    rm -rf /home/devasc/Documents/Git_testing/deployment/flask24
+    rm -rf /home/devasc/Documents/Git_testing/deployment/flask_venv
+    echo "Application folder has been removed"
+}
+trap cleanup EXIT
+python3 -m venv flask_venv
+source flask_venv/bin/activate
+git clone https://github.com/AdamAouinaChaoui/flask24
+cd flask24/
 pip install -r requirements.txt
 python3 sample_app.py
-deactivate
-cd ..
-rm -rf flask24

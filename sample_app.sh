52ed03e17d9429e9ed39eff4585925806969c0dc
AdamAouinaChaoui <145915167+AdamAouinaChaoui@users.noreply.github.com>
2024-12-04 16:42:21 +0100

    Update sample_app.sh

--- sample_app.sh
+++ sample_app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-staging exercise with jenkins
-python3 -m venv Flaskapp
+staging exercise with python environment van github
+python3 -m venv flask24
 source flask_venv/bin/activated
 git clone https://github.com/AdamAouinaChaoui/flask24.git
 pip install -r requirements.txt
 python3 sample_app.py
 deactivate
 cd ..
-rm -rf Flaskapp
+rm -rf flask24
